92d2af72ae2ecd037ca13490218e6309ccdf6c42
ysfaltun <79021092+ysfaltun@users.noreply.github.com>
2023-07-07 12:06:07 +0300

    Update but030119038.dart

--- but030119038.dart
+++ but030119038.dart
@@ -1,1 +1,5 @@
-
+import 'dart:convert';
+class But030119038{
+factory But030119038.fromJson(Stringsource) => But030119038.fromMap(json.decode(source));
+String toJson() => json.encode(toMap());
+  }
